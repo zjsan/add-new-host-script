dc9b6b8eef2fc3cd3cec8820bd1d87a86b89f1c4
zjsan <zjsantos25@gmail.com>
2026-01-28 11:41:35 +0800

    added admin privellege check and creating host entry logic

--- src/host.py
+++ src/host.py
@@ -14,4 +14,29 @@ def read_hosts_file():
     with open(get_hosts_path(), "r") as f:
         return f.read()
 
-def host_exist()
+def host_exist(domain,lines):
+
+    #remove whitespace and comments
+    for line in lines:
+        clean = line.strip()
+
+        #skip empty lines and comments
+        if clean and not clean.startswith("#"):
+
+            #split line into whiteespaces
+            #check if domain exists in a token, not substring
+            if domain in clean.split():
+                return True.
+    return False
+
+def add_host_entry(ip, domain):
+
+    line = read_hosts_file()#read the file
+
+    #check if host already exists
+    if host_exist(domain,lines):
+        return False #host already exists
+
+    lines.append(f"{ip}\t{domain}\n")
+
+
--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import sys 
+import ctypes
 
 production_ip = "13.251.136.207"
 production_domain = "app.sdg-dashboard.com"
@@ -10,33 +11,46 @@ def show_menu():
     print("[2] Remove host entry")
     print("[0] Exit")
 
+def is_admin():
+    try:
+       
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
 
 def main():
     while True:
-        show_menu()
-        choice = input("\n Select opton: ").strip()
 
-        if choice == "1":
-            success = add_host_entry(production_ip, production_domain)
+        if is_admin():
 
-            if success:
-                print(f"\n Host entry added: {production_ip} {production_domain}\n")
+            show_menu()
+            choice = input("\n Select opton: ").strip()
+
+            if choice == "1":
+                success = add_host_entry(production_ip, production_domain)
+
+                if success:
+                    print(f"\n Host entry added: {production_ip} {production_domain}\n")
+                else:
+                    print(f"\n Host failed to add: {production_ip} {production_domain}\n")
+
+            elif choice == "2":
+                success = remove_host_entry(production_domain)
+
+                if success:
+                    print(f"\n Host entry removed: {production_domain}\n")
+                else:
+                    print(f"\n Host failed to remove: {production_domain}\n")
+
+            elif choice == "0":
+                print("\n Exiting...\n")
+                sys.exit(0)
             else:
-                print(f"\n Host failed to add: {production_ip} {production_domain}\n")
-
-        elif choice == "2":
-            success = remove_host_entry(production_domain)
-
-            if success:
-                print(f"\n Host entry removed: {production_domain}\n")
-            else:
-                print(f"\n Host failed to remove: {production_domain}\n")
-
-        elif choice == "0":
-            print("\n Exiting...\n")
-            sys.exit(0)
+                print("\n Invalid option. Please try again.\n")
         else:
-            print("\n Invalid option. Please try again.\n")
+            print("This script requires administrative privileges to run.")
+            print("Please run the script as an administrator.")
+            sys.exit(1)
 
 if __name__ == "__main__":
     main()
